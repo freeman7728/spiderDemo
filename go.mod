7ad5b38f628f94c6caf8a3d9ac532192b24aaa0d
freeman7728 <1357880399@qq.com>
2024-08-29 19:32:52 +0800

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module spiderDemo
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,20 @@
+/*
+ * @Description:
+ * @author: freeman7728
+ * @Date: 2024-08-29 19:28:02
+ * @LastEditTime: 2024-08-29 19:29:59
+ * @LastEditors: freeman7728
+ */
+package main
+
+func main() {
+
+}
+
+//TODO 发送请求
+
+//TODO 解析网页
+
+//TODO 获取节点
+
+//TODO 保存信息
